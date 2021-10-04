c186bfae5cd496d7a06e46a18bb3674bd7963677
Adam Howes <adamhowes@outlook.com>
2021-10-04 16:41:03 +0100

    Add develop_start plus and develop_clean plus (not sure that develop_start plus actually works though)

--- shrtcts.R
+++ shrtcts.R
@@ -1,7 +1,20 @@
-#' Say Something Nice
+#' `orderly` develop start plus
 #'
-#' A demo of cool things.
+#' Like `orderly::orderly_develop_start` but also changes the directory:
+#' if you have `report/script.R` then it will set your directory to that file,
+#' as well as calling `orderly::orderly_develop_start("report")`.
 #'
-#' @interactive
-#' @shortcut Ctrl+Alt+P
-praise::praise
+#' @shortcut Ctrl+D
+file <- rstudioapi::getSourceEditorContext()$path
+orderly_report <- dirname(file)
+orderly::orderly_develop_start(orderly_report)
+setwd(orderly_report)
+
+#' `orderly` develop clean plus
+#'
+#' Like `orderly::orderly_develop_clean` but for every report in your repository.
+#'
+#' @shortcut Ctrl+T
+setwd(rprojroot::find_rstudio_root_file())
+tasks <- list.files("src")
+lapply(tasks, orderly::orderly_develop_clean)
